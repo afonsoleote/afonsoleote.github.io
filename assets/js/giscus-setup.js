8df3741fc43687484ef658e7d87d73b818207285
afonsoleote <145656860+afonsoleote@users.noreply.github.com>
2025-12-25 21:02:36 +0000

    Deploying to gh-pages from @ afonsoleote/afonsoleote.github.io@74d28c868cb98721119e04d074ea4525fa4b2446 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "afonsoleote/site",
+    "data-repo": "afonsoleote/afonsoleote.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
